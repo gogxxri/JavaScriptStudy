e2ba01588b5decfa9e3881e984ee82d2482dc3ba
gyuri <rbfl7645@naver.com>
2024-07-02 00:59:15 +0900

    blog published, delete

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,60 +3,99 @@ import {useState} from 'react';
 
 function App() {
 
-  let [title, setTitle] = useState(['남자 코트 추천', '여자 원피스 추천', '데이트 코스 추천']);
-  let [like, setLike] = useState([0, 0, 0]);
-  let [modal, setModal] = useState([false, false, false]);
+  let [posts, setPosts] = useState([
+    {
+      title : '남자 코트 추천',
+      likes : 0,
+      date : '2024-06-15'
+    },
+    {
+      title : '여자 원피스 추천',
+      likes : 0,
+      date : '2024-06-17'
+    },
+    {
+      title : '데이트 코스 추천',
+      likes : 0,
+      date : '2024-06-30'
+    },
+  ])
+  let [modal, setModal] = useState(false);
+  let [modalTitle, setModalTitle]= useState('');
+  let [inputValue, setInputValue] = useState('');
 
-  const editTitle = () => {
-    let copy = [...title];
-    copy[0] = '여자 코트 추천';
-    setTitle(copy);
+  const handleLike = (idx) => {
+    let updatedPosts = [...posts];
+    updatedPosts[idx].likes++;
+    setPosts(updatedPosts);
+  };
+
+  const handleDelete = (idx) => {
+    let updatePosts = [...posts];
+    updatePosts.splice(idx,1);
+    setPosts(updatePosts);
   }
+
+  const handlePublish = () => {
+    if(inputValue !== ''){
+      let updatePosts = [{
+        title : inputValue,
+        likes : 0,
+        date : new Date().toISOString().slice(0, 10)
+      }, ...posts];
+      setPosts(updatePosts);
+      setInputValue('');
+    }
+  };
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>BLOG</h4>
+        <h4>Blog</h4>
       </div>
 
-      <button onClick={()=> {
-        let ganada = [...title];
-        ganada.sort();
-        setTitle(ganada)
-      }}> 가나다 순 정렬 </button>
+      <button onClick={()=>{
+        let copy = [...posts];
+        copy.sort((a, b) => a.title.localeCompare(b.title));
+        setPosts(copy);
+      }}>가나다순 정렬</button>
 
-      <button onClick={() => {
-        editTitle();      
-      }}> 글 수정 </button>
+      {posts.map((post, i) => (
+        <div className="list" key={i}>
+          <h4 onClick={() => {
+            setModal(true);
+            setModalTitle(post.title);
+          }}>
+            {post.title}
+            <span onClick={() => {
+              handleLike(i)
+            }}> 👍 {post.likes}</span>
+          </h4>
+          <p>{post.date}</p>
+          <button onClick={()=>{
+            handleDelete(i)
+          }}>삭제</button>
+        </div>
+      ))} 
+      <input value={inputValue} // inputValue 상태를 input 요소의 value로 설정
+        onChange={(e)=>{
+        setInputValue(e.target.value)
+      }}/>
+      <button onClick={handlePublish}>글 발행</button>     
 
-        {title.map(function(title, i){
-          return (<div className="list" key={i}>
-            <h4 onClick={() => {
-              let changeModal = [...modal];
-              changeModal[i] = !changeModal[i];
-              setModal(changeModal);
-            }}>{title}
-              <span onClick={(event)=>{
-                event.stopPropagation(); // 클릭 이벤트 전파 중지
-                let copy = [...like];
-                copy[i] = copy[i]+1; 
-                setLike(copy)}}> 👍 </span> {like[i]} 
-            </h4>
-            <p>2월 16일 발행</p>
-            {modal[i] && <Modal title={title} editTitle={editTitle} />}
-          </div>)
-        })}
+      {modal && <Modal title={modalTitle} 
+      date={posts.find(p => p.title === modalTitle)?.date}  />}
     </div>
   );
+  
 }
 
 function Modal(props){
   return (
     <div className="modal">
       <h4>{props.title}</h4>
-      <p>날짜</p>
+      <p>{props.date}</p>
       <p>상세내용</p>
-      <button onClick={props.editTitle}>글 수정</button>
     </div>
   );
 }
